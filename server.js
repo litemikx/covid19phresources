1a60ff615a6726b5b553d0ceb98ffd30223b5f9d
Charmie Q <charmiequino@gmail.com>
2020-03-22 02:15:40 +0800

    remove ip

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ app.use((err, req, res, next) => {
 
 
 
-const listener = app.listen(process.env.PORT || 3000, '192.168.0.10', () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + process.env.PORT)
 })
